4bf4f58733414ca6d13ef7bcb455878f12c80c2d
Diego Santana <diegodanielsantana00@gmail.com>
2026-04-01 11:48:51 -0300

    feat: first commit

---
+++ src/main/java/com/danieldiego/trackMaintenance/TrackMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.danieldiego.trackMaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danieldiego/trackMaintenance/TrackMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieldiego.trackMaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
